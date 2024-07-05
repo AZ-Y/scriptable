9d286bfbf4e1ec9dfd94b413ce3bdea2d5f7b67f
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-05 23:01:16 +0800

    Update babycarerw.js

--- babycarerw.js
+++ babycarerw.js
@@ -12,175 +12,39 @@ const $ = new Env('bckid');
                 $.msg($.name, 'Token 获取失败', '未找到 Authorization 头');
             }
         } else {
-            $.log(`没有找到响应头: ${JSON.stringify($response)}`);
+            $.msg($.name, '响应无头部', '无有效的响应头部');
         }
     } catch (error) {
         $.log(`脚本运行出错: ${error}`);
+        $.msg($.name, '脚本错误', `错误信息: ${error}`);
     }
 })().finally(() => $.done());
 
 function Env(t, e) {
-    class s {
-        constructor(t) {
-            this.env = t
-        }
-        send(t, e = "GET") {
-            t = "string" == typeof t ? { url: t } : t;
-            let s = this.get;
-            return "POST" === e && (s = this.post), new Promise((e, i) => {
-                s.call(this, t, (t, s, r) => {
-                    t ? i(t) : e(s)
-                })
-            })
-        }
-        get(t) {
-            return this.send.call(this.env, t)
-        }
-        post(t) {
-            return this.send.call(this.env, t, "POST")
-        }
-    }
-    return new class {
-        constructor(t, e) {
-            this.name = t, this.http = new s(this), this.data = null, this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`)
-        }
-        isNode() {
-            return "undefined" != typeof module && !!module.exports
-        }
-        isQuanX() {
-            return "undefined" != typeof $task
-        }
-        isSurge() {
-            return "undefined" != typeof $httpClient && "undefined" == typeof $loon
-        }
-        isLoon() {
-            return "undefined" != typeof $loon
-        }
-        toObj(t, e = null) {
-            try {
-                return JSON.parse(t)
-            } catch (e) {
-                return e
-            }
-        }
-        toStr(t, e = null) {
-            try {
-                return JSON.stringify(t)
-            } catch (e) {
-                return e
-            }
-        }
-        getdata(t) {
-            let e = this.getval(t);
-            if (/^@/.test(t)) {
-                const [, s, r] = /^@(.*?)\.(.*?)$/.exec(t), i = s ? this.getval(s) : "";
-                if (i) try {
-                    const t = JSON.parse(i);
-                    e = t ? this.lodash_get(t, r, "") : e
-                } catch (t) {
-                    e = ""
-                }
-            }
-            return e
-        }
-        setdata(t, e) {
-            let s = !1;
-            if (/^@/.test(e)) {
-                const [, r, i] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(r), h = r ? "null" === o ? null : o || "{}" : "{}";
-                try {
-                    const e = JSON.parse(h);
-                    this.lodash_set(e, i, t), s = this.setval(JSON.stringify(e), r)
-                } catch (e) {
-                    const o = {};
-                    this.lodash_set(o, i, t), s = this.setval(JSON.stringify(o), r)
-                }
-            } else s = this.setval(t, e);
-            return s
-        }
-        getval(t) {
-            return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null
-        }
-        setval(t, e) {
-            return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null
-        }
-        initGotEnv(t) {
-            this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar))
-        }
-        get(t, e = (() => {})) {
-            t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, {
-                "X-Surge-Skip-Scripting": !1
-            })), $httpClient.get(t, (t, s, r) => {
-                !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r)
-            })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, {
-                hints: !1
-            })), $task.fetch(t).then(t => {
-                const {
-                    statusCode: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                } = t;
-                e(null, {
-                    status: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                }, o)
-            }, t => e(t && t.error || "UndefinedError")) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => {
-                try {
-                    if (t.headers["set-cookie"]) {
-                        const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();
-                        s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar
-                    }
-                } catch (t) {
-                    this.logErr(t)
-                }
-            }).then(t => {
-                const {
-                    statusCode: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                } = t;
-                e(null, {
-                    status: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                }, o)
-            }, t => {
-                const {
-                    message: s,
-                    response: r
-                } = t;
-                e(s, r, r && r.body)
-            }))
-        }
-        post(t, e = (() => {})) {
-            const s = t.method ? t.method.toLocaleLowerCase() : "post";
-            if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, {
-                "X-Surge-Skip-Scripting": !1
-            })), $httpClient[s](t, (t, s, r) => {
-                !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r)
-            });
-            else if (this.isQuanX()) t.method = s, this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, {
-                hints: !1
-            })), $task.fetch(t).then(t => {
-                const {
-                    statusCode: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                } = t;
-                e(null, {
-                    status: s,
-                    statusCode: r,
-                    headers: i,
-                    body: o
-                }, o)
-            }, t => e(t && t.error || "UndefinedError"));
-            else if (this.isNode()) {
-                this.initGotEnv(t);
-                const {
-                    url: i,
-                   
+    this.name = t;
+    this.logSeparator = '\n';
+    this.startTime = (new Date()).getTime();
+    this.data = {};
+    this.logs = [];
+    
+    this.getval = function(key) {
+        return this.data[key] || null;
+    };
+    
+    this.setval = function(value, key) {
+        return this.data[key] = value;
+    };
+    
+    this.log = function(...args) {
+        console.log(...args);
+        this.logs.push(args.join(this.logSeparator));
+    };
+    
+    this.msg = function(title, subtitle, body) {
+        console.log(`${title}: ${subtitle} - ${body}`);
+    };
+    
+    this.done = function() {
+        // 结束脚本
+    };
+}
