ba13fed3f49fa48fb658a5d805f55afea07bfa54
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-05 23:05:08 +0800

    Update babycare.js

--- babycare.js
+++ babycare.js
@@ -1,9 +1,5 @@
-#!name=Bckid 自动抓包
-#!desc=用于自动处理 Bckid API 的请求
-#!category=抓包
-
-[Script]
-bckid_获取token = type=http-response,pattern=^https:\/\/api\.bckid\.com\.cn\/operation\/front\/bonus\/userTask\/list,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js
+[rewrite_local]
+bckid_获取token = type=http-response,pattern=^https:\/\/api.bckid.com.cn\/operation\/front\/bonus\/userTask\/list,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js,requires-body=1,max-size=0
 
 [MITM]
 hostname = %APPEND% api.bckid.com.cn
