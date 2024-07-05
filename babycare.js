18325ffaa165050010c37d1a587f737484b197a3
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-05 22:42:49 +0800

    Update babycare.js

--- babycare.js
+++ babycare.js
@@ -1,6 +1,173 @@
 #!name=Bckid 自动抓包
 #!desc=用于自动处理 Bckid API 的请求
 #!category=抓包
+  const $ = new Env('bckid');
+
+!(async () => {
+    if ($request && $request.headers) {
+        const token = $request.headers['Authorization'];
+        if (token) {
+            $.log(`获取到的 token: ${token}`);
+            $.setdata(token, 'bckid_token');
+            $.msg($.name, 'Token 获取成功', token);
+        } else {
+            $.msg($.name, 'Token 获取失败', '未找到 Authorization 头');
+        }
+    }
+})().finally(() => $.done());
+
+function Env(t, e) {
+    class s {
+        constructor(t) {
+            this.env = t
+        }
+        send(t, e = "GET") {
+            t = "string" == typeof t ? {
+                url: t
+            } : t;
+            let s = this.get;
+            return "POST" === e && (s = this.post), new Promise((e, i) => {
+                s.call(this, t, (t, s, r) => {
+                    t ? i(t) : e(s)
+                })
+            })
+        }
+        get(t) {
+            return this.send.call(this.env, t)
+        }
+        post(t) {
+            return this.send.call(this.env, t, "POST")
+        }
+    }
+    return new class {
+        constructor(t, e) {
+            this.name = t, this.http = new s(this), this.data = null, this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`)
+        }
+        isNode() {
+            return "undefined" != typeof module && !!module.exports
+        }
+        isQuanX() {
+            return "undefined" != typeof $task
+        }
+        isSurge() {
+            return "undefined" != typeof $httpClient && "undefined" == typeof $loon
+        }
+        isLoon() {
+            return "undefined" != typeof $loon
+        }
+        toObj(t, e = null) {
+            try {
+                return JSON.parse(t)
+            } catch (e) {
+                return e
+            }
+        }
+        toStr(t, e = null) {
+            try {
+                return JSON.stringify(t)
+            } catch (e) {
+                return e
+            }
+        }
+        getdata(t) {
+            let e = this.getval(t);
+            if (/^@/.test(t)) {
+                const [, s, r] = /^@(.*?)\.(.*?)$/.exec(t), i = s ? this.getval(s) : "";
+                if (i) try {
+                    const t = JSON.parse(i);
+                    e = t ? this.lodash_get(t, r, "") : e
+                } catch (t) {
+                    e = ""
+                }
+            }
+            return e
+        }
+        setdata(t, e) {
+            let s = !1;
+            if (/^@/.test(e)) {
+                const [, r, i] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(r), h = r ? "null" === o ? null : o || "{}" : "{}";
+                try {
+                    const e = JSON.parse(h);
+                    this.lodash_set(e, i, t), s = this.setval(JSON.stringify(e), r)
+                } catch (e) {
+                    const o = {};
+                    this.lodash_set(o, i, t), s = this.setval(JSON.stringify(o), r)
+                }
+            } else s = this.setval(t, e);
+            return s
+        }
+        getval(t) {
+            return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null
+        }
+        setval(t, e) {
+            return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null
+        }
+        initGotEnv(t) {
+            this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar))
+        }
+        get(t, e = (() => {})) {
+            t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, {
+                "X-Surge-Skip-Scripting": !1
+            })), $httpClient.get(t, (t, s, r) => {
+                !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r)
+            })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, {
+                hints: !1
+            })), $task.fetch(t).then(t => {
+                const {
+                    statusCode: s,
+                    statusCode: r,
+                    headers: i,
+                    body: o
+                } = t;
+                e(null, {
+                    status: s,
+                    statusCode: r,
+                    headers: i,
+                    body: o
+                }, o)
+            }, t => e(t && t.error || "UndefinedError")) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => {
+                try {
+                    if (t.headers["set-cookie"]) {
+                        const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();
+                        s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar
+                    }
+                } catch (t) {
+                    this.logErr(t)
+                }
+            }).then(t => {
+                const {
+                    statusCode: s,
+                    statusCode: r,
+                    headers: i,
+                    body: o
+                } = t;
+                e(null, {
+                    status: s,
+                    statusCode: r,
+                    headers: i,
+                    body: o
+                }, o)
+            }, t => {
+                const {
+                    message: s,
+                    response: r
+                } = t;
+                e(s, r, r && r.body)
+            }))
+        }
+        post(t, e = (() => {})) {
+            const s = t.method ? t.method.toLocaleLowerCase() : "post";
+            if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, {
+                "X-Surge-Skip-Scripting": !1
+            })), $httpClient[s](t, (t, s, r) => {
+                !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r)
+            });
+            else if (this.isQuanX()) t.method = s, this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, {
+                hints: !1
+            })), $task.fetch(t).then(t => {
+                const {
+                    statusCode: s,
+                    statusCode
 
 [Script]
 bckid_获取token = type=http-response,pattern=^https:\/\/api\.bckid\.com\.cn\/operation\/front\/bonus\/userTask\/list,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js
