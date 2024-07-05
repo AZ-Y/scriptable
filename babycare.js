fb202f75b45386c919b426c0e991f9601cf208ea
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-05 16:13:07 +0800

    Update babycare.js

--- babycare.js
+++ babycare.js
@@ -3,7 +3,7 @@
 #!category=抓包
 
 [Script]
-bckid_获取token = type=http-response,pattern=^https:\/\/api.bckid.com.cn\/operation\/front\/bonus\/userTask\/list,script-path=https://raw.githubusercontent.com/yang7758258/ohhh154/main/babycare.js,requires-body=1,max-size=0
+bckid_获取token = type=http-response,pattern=^https:\/\/api.bckid.com.cn\/operation\/front\/bonus\/userTask\/list,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js,requires-body=1,max-size=0
 
 [MITM]
 hostname = %APPEND% api.bckid.com.cn
