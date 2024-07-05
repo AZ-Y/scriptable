b034e0d5416ed6620b96018e6ad8ce775be3879d
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-05 23:23:36 +0800

    Update babycare.js

--- babycare.js
+++ babycare.js
@@ -1,5 +1,4 @@
 [rewrite_local]
 bckid_获取token = type=http-response,pattern=^https:\/\/api.bckid.com.cn\/operation\/front\/bonus\/userTask\/list,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js,requires-body=1,max-size=0
-
 [MITM]
 hostname = api.bckid.com.cn
