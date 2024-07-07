f7858e26cd0a572c9c22463747d9a6f5c1af7a48
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-07 19:34:51 +0800

    Update yadeack.js

--- yadeack.js
+++ yadeack.js
@@ -4,7 +4,7 @@
 
 [Script]
 雅迪星球_获取cookie = type=http-response,pattern=^https:\/\/lwapp\.yzw\.cn\/api\/Home\/GetWorkerInfo,script-path=https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/YZGJ/YZGJ.js,requires-body=1,max-size=0
-雅迪星球 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/YZGJ/YZGJ.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+雅迪星球 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/yadea.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
 hostname = %APPEND% opmd.yadea.com.cn
