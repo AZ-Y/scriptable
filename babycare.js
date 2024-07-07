e76d89eb28d224a46c2501545d0721cc4d6d0c3e
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-08 01:05:15 +0800

    Update babycarerw.js

--- babycare.js
+++ babycare.js
@@ -1,4 +1,10 @@
-[rewrite_local]
-bckid_获取token = type=http-response,pattern=^https:\/\/api.bckid.com.cn\/operation\/front\/bonus\/userTask\/list,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js,requires-body=1,max-size=0
+#!name=babycare小程序签到
+#!desc=
+#!category=羊毛
+
+[Script]
+babycare_获取cookie = type=http-response,pattern=^https:\/\/api\.bckid\.com\.cn\/operation\/front\/bonus\/userSign\/v3\/sign,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/yadearw.js,requires-body=1,max-size=0
+babycare = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/yadearw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+
 [MITM]
 hostname = api.bckid.com.cn
