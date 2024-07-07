87a08eef38f42c43030b08ba6d64d920196c110a
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-07 19:36:08 +0800

    Update yadeack.js

--- yadeack.js
+++ yadeack.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-雅迪星球_获取cookie = type=http-response,pattern=^https:\/\/lwapp\.yzw\.cn\/api\/Home\/GetWorkerInfo,script-path=https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/YZGJ/YZGJ.js,requires-body=1,max-size=0
+雅迪星球_获取cookie = type=http-response,pattern=^https:\/\/lwapp\.yzw\.cn\/api\/Home\/GetWorkerInfo,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/yadea.js,requires-body=1,max-size=0
 雅迪星球 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/yadea.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
