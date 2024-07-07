507cec7168fc3e1c22f57a6637010492ca94436b
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-08 06:56:37 +0800

    Update babycareck.js

--- babycareck.js
+++ babycareck.js
@@ -7,4 +7,4 @@ babycare_获取cookie = type=http-response,pattern=^https:\/\/api\.bckid\.com\.c
 babycare = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/babycarerw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
-hostname = api.bckid.com.cn
+hostname = %APPEND% api.bckid.com.cn
