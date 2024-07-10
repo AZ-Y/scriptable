f1d7a94e1437c282ceafe7a8f8b9f5ce3dc3d6c7
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-10 08:40:18 +0800

    Update meiyouck.js

--- meiyouck.js
+++ meiyouck.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-美柚_获取cookie = type=http-response,pattern=^https:\/\/member\.guoyuejiu\.com\/api\/user\/history,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js,requires-body=1,max-size=0
+美柚_获取cookie = type=http-response,pattern=^https:\/\/coin-api\.youzibuy\.com\/checkin_v3,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js,requires-body=1,max-size=0
 美柚 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
