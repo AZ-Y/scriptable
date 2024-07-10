c057cee3bf7739136f8ec5b6586b7f8ccdb25a18
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-10 23:39:15 +0800

    Update gljjck.js

--- gljjck.js
+++ gljjck.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-囯乐酱酒_获取cookie = type=http-response,pattern=^https:\/\/member\.guoyuejiu\.com\/api\/user\/history,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,requires-body=1,max-size=0
+囯乐酱酒_获取cookie = type=http-response,pattern=^https:\/\/member\.guoyuejiu\.com\/api\/sign\/daily\/sign$,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,requires-body=1,max-size=0
 囯乐酱酒 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
