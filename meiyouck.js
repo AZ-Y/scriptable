d3bb81a5b1b983435d45baa46615974ca0399357
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-11 19:13:54 +0800

    Update meiyouck.js

--- meiyouck.js
+++ meiyouck.js
@@ -3,8 +3,11 @@
 #!category=羊毛
 
 [Script]
-美柚_获取cookie = type=http-response,pattern=^https:\/\/coin-api\.youzibuy\.com\/api\/member_task\/report script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js,requires-body=1,max-size=0
-美柚 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+# 获取美柚的cookie
+美柚_获取cookie = type=http-response, pattern=^https:\/\/coin-api\.youzibuy\.com\/api\/member_task\/report, script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js, requires-body=1, max-size=0
+
+# 定时签到任务
+美柚 = type=cron, cronexp="8 8,16 * * *", script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js, script-update-interval=0, timeout=3600, wake-system=1, engine=jsc
 
 [MITM]
 hostname = coin-api.youzibuy.com
