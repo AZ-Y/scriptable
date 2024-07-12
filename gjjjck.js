0ff347f14009c9232df97cecf0b912ad9d274a85
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-12 21:11:29 +0800

    Update gjjjck.js

--- gjjjck.js
+++ gjjjck.js
@@ -3,8 +3,8 @@
 #!category=羊毛
 
 [Script]
-顾家家居_获取cookie = type=http-response,pattern=^https:\/\/mc\.kukahome\.com\/club-server\/member\/insertMemberLogin,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gjjjrw.js,requires-body=1,max-size=0
+顾家家居_获取cookie = type=http-response,pattern=^https:\/\/mc\.kukahome\.com\/.*,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gjjjrw.js,requires-body=1,max-size=0
 顾家家居 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gjjjrw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
-hostname =  mc.kukahome.com
+hostname = mc.kukahome.com
