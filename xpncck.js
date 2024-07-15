dbeefaba6d4a4347e9a82896ed75490d77795621
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-15 19:03:22 +0800

    Update xpncck.js

--- xpncck.js
+++ xpncck.js
@@ -3,8 +3,7 @@
 #!category=羊毛
 
 [Script]
-兴攀农场_获取cookie = type=http-response,pattern=^https:\/\/p\.xpfarm\.cn\/treemp\/user\.
-PersonalCenter\/getInfo,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/xpnc.js,requires-body=1,max-size=0
+兴攀农场_获取cookie = type=http-response,pattern=^https:\/\/p\.xpfarm\.cn\/treemp\/user\.PersonalCenter\/getInfo,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/xpnc.js,requires-body=1,max-size=0
 兴攀农场 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/xpnc.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
