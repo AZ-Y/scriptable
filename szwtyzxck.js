b3c197558bb9bccbe882a7982f18bf30c6686d3d
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-18 23:12:54 +0800

    Update szwtyzxck.js

--- szwtyzxck.js
+++ szwtyzxck.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-深圳湾体育中心_获取cookie = type=http-response,pattern=^https:\/\/program\.springcocoon\.com\/szbay\/api\/services\/app\/SignInRecord\/SignInAsync$,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/szwtyzx.js,requires-body=1,max-size=0
+深圳湾体育中心_获取cookie = type=http-response,pattern=^https:\/\/program\.springcocoon\.com\/szbay\/api\/services\/app,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/szwtyzx.js,requires-body=1,max-size=0
 深圳湾体育中心 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/szwtyzx.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
