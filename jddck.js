36b3adf6b344daa352085fbf303dffcca748646b
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-23 12:18:29 +0800

    Update jddck.js

--- jddck.js
+++ jddck.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-金杜丹_获取cookie = type=http-response,pattern=^https:\/\/tianxin\.jmd724\.com\/index,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jdd.js,requires-body=1,max-size=0
+金杜丹_获取cookie = type=http-response,pattern=^https:\/\/tianxin\.jmd724\.com\/index\.php\?r=client\/v1\/task,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jdd.js,requires-body=1,max-size=0
 金杜丹 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jdd.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
