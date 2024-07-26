ebcae10c16d955943ac9d79a3ebfddf5dc612d17
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-26 10:28:45 +0800

    Update qchscck.js

--- qchscck.js
+++ qchscck.js
@@ -3,7 +3,7 @@
 #!category=羊毛
 
 [Script]
-七彩虹商城_获取cookie = type=http-response,pattern=^https:\/\/hdyx\.by\-health\.com\/taskCenter,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/qchsc.js,requires-body=1,max-size=0
+七彩虹商城_获取cookie = type=http-response,pattern=^https:\/\/hdyx\.by\-health\.com\/taskCenter\/api\/sign,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/qchsc.js,requires-body=1,max-size=0
 七彩虹商城 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/qchsc.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
