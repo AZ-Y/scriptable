bf89ccbef8d63bccc0924d773b089d2fd69935eb
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-26 23:07:04 +0800

    Create jmzhsck.js

---
+++ jmzhsck.js
@@ -0,0 +1,10 @@
+【Quantumult X】
+-----------------
+[rewrite_local]
+https:\/\/wxavip\-up\.ezrpro\.cn\/Vip url script-request-body https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/jiumu/jiumu.js
+
+[task_local]
+5 0 * * * https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/jiumu/jiumu.js, tag=九木杂物社
+
+【All App MitM】
+hostname = wxavip-up.ezrpro.cn
