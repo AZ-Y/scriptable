c253aa265ded9515d8d244fe1e81b42297f73463
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-27 10:17:55 +0800

    Update jmzhsck.js

--- jmzhsck.js
+++ jmzhsck.js
@@ -2,7 +2,7 @@
         请勿转载与贩卖！⚠️⚠️⚠️
 
 [Script]
-http-request ^https:\/\/openapi\.jmzawushe\.com\/member\/signRecord script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jmzhsck.js, timeout=10, enabled=true, tag=九木杂货社Cookies
+http-request ^https:\/\/openapi\.jmzawushe\.com\/member\/signRecord script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jmzhs.js, timeout=10, enabled=true, tag=九木杂货社Cookies
 
 
 [Task]
