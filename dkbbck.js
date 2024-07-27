7d23c9e57b030ba75a63c57d7e4fc2a56a904597
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-07-27 17:14:19 +0800

    Update dkbbck.js

--- dkbbck.js
+++ dkbbck.js
@@ -1,5 +1,5 @@
 [Script]
-登康贝比商城_获取cookie = type=http-response,pattern=^https:\/\/hx\.hemiao100\.com\/backg\/signed,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,requires-body=1,max-size=0
+登康贝比商城_获取cookie = type=http-response,pattern=^https:\/\/hx\.hemiao100\.com\/backg,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,requires-body=1,max-size=0
 登康贝比商城 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
