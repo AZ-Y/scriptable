3be2f0f90347092e4e2f6e67ba80b9ac67f09589
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-07 23:29:50 +0800

    Update meiyouck.js

--- meiyouck.js
+++ meiyouck.js
@@ -5,9 +5,6 @@
 [Script]
 # 获取美柚的cookie
 美柚_获取cookie = type=http-response, pattern=^https:\/\/coin\-api\.youzibuy\.com\/checkin_v3, script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js, requires-body=1, max-size=0
-
-# 定时签到任务
-美柚 = type=cron, cronexp="8 8,16 * * *", script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/meiyourw.js, script-update-interval=0, timeout=3600, wake-system=1, engine=jsc
-
+  
 [MITM]
 hostname = coin-api.youzibuy.com
