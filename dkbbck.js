ea0b9df5a20154b3094cb392199368fee2dce629
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-12 14:38:44 +0800

    Update dkbbck.js

--- dkbbck.js
+++ dkbbck.js
@@ -1,6 +1,6 @@
 [Script]
-登康贝比商城_获取cookie = type=http-response,pattern=^https:\/\/hx\.hemiao100\.com\/backg\/signed\/hmSignedActivityInfo\/small,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,requires-body=1,max-size=0
-登康贝比商城 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+登康贝比_获取cookie = type=http-response,pattern=^https:\/\/hx\.hemiao100\.com\/backg\/signed\/hmSignedActivityInfo\/small,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,requires-body=1,max-size=0
+登康贝比 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/dkbb.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
 hostname =  hx.hemiao100.com
