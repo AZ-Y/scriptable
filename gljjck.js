eb60b5512ada6f16692d83b0bdb7a3a203ce739b
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-12 14:02:26 +0800

    Update gljjck.js

--- gljjck.js
+++ gljjck.js
@@ -1,10 +1,10 @@
-#!name=囯乐酱酒
-#!desc=囯乐酱酒
+#!name=
+#!desc=
 #!category=羊毛
 
 [Script]
-囯乐酱酒_获取cookie = type=http-response,pattern=^https:\/\/member\.guoyuejiu\.com\/api\/user\/score\?current=1&size=20,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,requires-body=1,max-size=0
-囯乐酱酒 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+囯乐_获取cookie = type=http-response,pattern=^https:\/\/member\.guoyuejiu\.com\/api\/sign,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,requires-body=1,max-size=0
+囯乐 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/gljjrw.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
 
 [MITM]
 hostname =  member.guoyuejiu.com
