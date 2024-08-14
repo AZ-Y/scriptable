4d376406c3d5a55b12eb643497166f7d0ef063e2
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-14 23:25:09 +0800

    Create jgpyck.js

---
+++ jgpyck.js
@@ -0,0 +1,6 @@
+[Script]
+朋友_获取cookie = type=http-response,pattern=^https:\/\/smp\-api\.iyouke\.com\/dtapi\/pointsSign,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jgpy.js,requires-body=1,max-size=0
+朋友 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jgpy.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
+
+[MITM]
+hostname =  smp-api.iyouke.com
