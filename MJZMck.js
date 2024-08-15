98f4e507babf6bf15c821cacd9cdb4dcbe945ca3
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-15 22:28:57 +0800

    Delete MJZMck.js

--- MJZMck.js
+++
@@ -1,6 +0,0 @@
-[Script]
-明基_获取cookie = type=http-response,pattern=^https:\/\/wit\-api\.benq\.com\.cn\/score,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/MJZM.js,requires-body=1,max-size=0
-明基 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/MJZM.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
-
-[MITM]
-hostname =  wit-api.benq.com.cn
