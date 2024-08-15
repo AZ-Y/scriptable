4797bb5cd0062890cf2f881a3d380667cb37d504
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-15 22:12:56 +0800

    Delete OPZMck.js

--- OPZMck.js
+++
@@ -1,6 +0,0 @@
-[Script]
-欧普_获取cookie = type=http-response,pattern=^https:\/\/kfscrm\.opple\.com\/opple\/scrm\/mkt\/activities,script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/OPZM.js,requires-body=1,max-size=0
-欧普 = type=cron,cronexp="8 8,16 * * *",script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/OPZM.js,script-update-interval=0,timeout=3600,wake-system=1,engine=jsc
-
-[MITM]
-hostname =  kfscrm.opple.com
