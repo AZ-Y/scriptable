73918f49cc02978a8e8893a7c4930fafdcdd7f96
AZ <101546956+AZ-Y@users.noreply.github.com>
2024-08-15 21:45:42 +0800

    Update jmzhs.js

--- jmzhs.js
+++ jmzhs.js
@@ -1,3 +1,16 @@
+/*
+------------------------------------------
+[Script]
+http-request ^https:\/\/openapi\.jmzawushe\.com\/member\/signRecord script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jmzhs.js, timeout=10, enabled=true, tag=九木杂货社Cookies
+
+
+[Task]
+cron "30 6 * * *" script-path=https://raw.githubusercontent.com/AZ-Y/scriptable/main/jmzhsck.js, timeout=3600, tag=九木杂物社
+
+
+[MITM]
+hostname = openapi.jmzawushe.com
+====================================*/
 const $ = new Env('九木杂物社签到');
 const jiumu = ($.isNode() ? JSON.parse(process.env.jiumu || '[]') : $.getjson("jiumu", []));
 let Utils = undefined;
